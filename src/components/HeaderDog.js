c521d949444a6765b626fd80540a9627469d3748
Javier <91976558+ElusiveMirage@users.noreply.github.com>
2022-11-11 20:12:15 +0800

    Updated css

--- src/components/HeaderDog.js
+++ src/components/HeaderDog.js
@@ -4,18 +4,6 @@ import { BsHouse } from 'react-icons/bs';
 
 import  Modal  from 'react-modal'
 
-const customStyles = {
-    content : {
-      top                   : '50%',
-      left                  : '50%',
-      right                 : 'auto',
-      bottom                : 'auto',
-      marginRight           : '-50%',
-      transform             : 'translate(-50%, -50%)',
-      backgroundColor: '#264653'
-    }
-  };
-
 function refreshPage() {
     window.location.reload();
   }
@@ -47,7 +35,7 @@ const Header = () => {
         <Modal
             isOpen={modalIsOpen}
             onRequestClose={() => setModalIsOpen(false)}
-            style={customStyles} >
+            className="Modal">
             <h1>Welcome to Dog Fun <BsHouse /></h1>
             <h2>A simple Web App for dog lovers that displays a random assortment of cute doggo pics.</h2>
             <hr></hr>
